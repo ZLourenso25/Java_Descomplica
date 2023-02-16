29efcb3962dc9fe8634804dfec042effcb5edf0d
Jose Lourenço M. Salviano <113633459+ZLourenso25@users.noreply.github.com>
2023-02-16 12:20:17 -0300

    new
    calculo de media utilizando o if e o else

---
+++ Principal10.java
@@ -0,0 +1,39 @@
+//Calculando media dos alunos
+import java.util.Scanner;
+
+public class Principal10 {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in); //imput está sendo utilizado para pode escrever com o reclado
+				
+		double nota1, nota2, nota3, media;
+		
+		System.out.println("Digite o valor da nota 1: ");
+		nota1 = input.nextDouble();
+		
+		System.out.println("Digite o valor da nota 2: ");
+		nota2 = input.nextDouble();
+		
+		System.out.println("Digite o valor da nota 3: ");
+		nota3 = input.nextDouble();
+		
+		media = (nota1 + nota3 + nota3)/3;
+		
+		System.out.println("A media é igual a " + media);
+		
+		if(media>=7) {
+			System.out.println("Aprovado");
+			}
+		else {
+			if(media<4) {
+				System.out.println("Reprovado");
+			}
+			else {
+				System.out.println("Final");
+
+			}
+		}
+	}
+
+}
