6abcfdf06552159a4b1b16b999fd9b5a5bd76703
Jose Lourenço M. Salviano <113633459+ZLourenso25@users.noreply.github.com>
2023-02-21 22:16:19 -0300

    commit
    inicio do camando while e do while

---
+++ Principal12.java
@@ -0,0 +1,16 @@
+//Uso do comando While comando de repetição que mantem a repetição enquanto a condição for verdadeira
+public class Principal12 {
+
+	public static void main(String[] args) {
+
+		int numero = 1;
+		
+		while(numero <=5) {
+		
+			System.out.println(numero);
+			
+			numero++;//uso do comando incremento
+		}
+	}
+
+}
---
+++ Principal13.java
@@ -0,0 +1,18 @@
+//Implementação do comando do while. difença e que o do
+//executa no final a condição fazendo assim com que o comando seja executado pelo menos uma vez
+public class Principal13 {
+
+	public static void main(String[] args) {
+		
+		int numero = 1;
+		
+		do {
+			System.out.println(numero);
+			
+			numero++;//uso do comando incremento
+			
+		}while(numero <=5);
+	
+	}
+
+}
