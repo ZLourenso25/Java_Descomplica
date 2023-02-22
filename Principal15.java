58897d9024b0995d5255d4b5822c243332817a60
Jose Lourenço M. Salviano <113633459+ZLourenso25@users.noreply.github.com>
2023-02-22 20:43:52 -0300

    Add files via upload
    aprendendo sobre o for

---
+++ Principal15.java
@@ -0,0 +1,12 @@
+//Aprendendo sobre o comando if
+public class Principal15 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=10; i++) {
+			
+			System.out.println(i);
+			
+		}
+	}
+}
