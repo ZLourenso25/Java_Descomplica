6c2588fc8201565b2a2fc408d83aef075178a968
Jose Lourenço M. Salviano <113633459+ZLourenso25@users.noreply.github.com>
2023-02-26 12:05:10 -0300

    Add files via upload

---
+++ Principal19.java
@@ -0,0 +1,14 @@
+//usando array
+public class Principal19 {
+
+	public static void main(String[] args) {
+		
+		int [] nota = {9,10,8};
+		
+		for(int i=0;i<3;i++) {
+			
+		System.out.println(nota[i]);
+		
+		}
+	}
+}
