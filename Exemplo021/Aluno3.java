81d39ffee526d132d6e3f60c81f29908992cd3ff
Jose Lourenço M. Salviano <113633459+ZLourenso25@users.noreply.github.com>
2023-03-23 10:43:16 -0300

    Add files via upload

---
+++ Exemplo021/Aluno3.java
@@ -0,0 +1,12 @@
+
+public class Aluno3 {
+	
+	String nome;
+	double teste, prova;
+	
+	double obterMedia() {
+		return (teste + prova)/2;
+		
+	}
+
+}
---
+++ Exemplo021/Principal21.java
@@ -0,0 +1,16 @@
+
+public class Principal21 {
+
+	public static void main(String[] args) {
+		
+		Aluno3 objeto = new Aluno3();
+		
+		objeto.nome ="José Lourenço";
+		objeto.teste = 9;
+		objeto.prova = 10;
+		
+		System.out.println(objeto.obterMedia());
+		
+	}
+
+}
